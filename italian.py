ef127e4afdcadb0b1fc37ace1fb872f00f5bf0ee
xzu048 <zongxzxo@gmail.com>
2014-03-12 23:27:36 -0500

    Update italian.py

--- italian.py
+++ italian.py
@@ -20,3 +20,8 @@ sage
 olive oil
 garlic
 thyme
+
+
+//sources: http://cooking.stackexchange.com/questions/1485/what-herbs-and-spices-are-in-italian-seasoning
+//http://www.calascio.com/spices&.htm
+//http://www.cafemancini.com/spices-used-in-italian-cuisine.php
