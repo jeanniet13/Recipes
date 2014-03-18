869eef80efcfdfdd8825857d18bd6d7e3252c716
jeanniet13 <jeanniet13@gmail.com>
2014-03-17 21:49:44 -0500

    Started on vegetarian transformation
    Veg trans current checks if recipe is already vegetarian. Need more
    clarification regarding veg -> meat trans.
    Made a bunch of changes to the other text files, and added a bunch more.
    
    Parsing now adds the ingredient type.
    Can't do tools because tools list is still really incomplete.

--- Recipes.py
+++ Recipes.py
@@ -18,7 +18,7 @@ class Ingredient:
     name = 'none'
     descriptor = ''
     preparation = ''
-    itype = ''
+    itype = '' #meat, spice, liquid, veggie, oil
 
 class Step:
     text = []
@@ -27,15 +27,19 @@ class Step:
     ingredients = []
     cooking_time = 0.0
 
-def Parse():
+def parse(link, recipe):
     cmp_list = open('cookingmethods_primary.txt', 'rb').read().split('\r\n')
     cms_list = open('cookingmethods_secondary.txt', 'rb').read().split('\r\n')
+    tool_list = open('tools.txt','rb').read().split('\r\n')
+    veg_list = open('vegetables.txt', 'rb').read().split('\r\n')
+    meat_list = open('meat.txt', 'rb').read().split('\r\n')
+    oil_list = open('oil.txt', 'rb').read().split('\r\n')
+    liquid_list = open('liquid.txt', 'rb').read().split('\r\n')
+    spice_list = open('spices.txt', 'rb').read().split('\r\n')
 
-    recipe_url = urllib2.urlopen("http://allrecipes.com/Recipe/Worlds-Best-Lasagna/Detail.aspx?evt19=1")
+    recipe_url = urllib2.urlopen("http://allrecipes.com/Recipe/Quinoa-and-Black-Beans/Detail.aspx?evt19=1")
     recipe_html = recipe_url.read()
     soup = BeautifulSoup(recipe_html)
-
-    recipe = Recipe()
 
     for quantity, ingredient in zip(soup.find_all(id='lblIngAmount'), soup.find_all(id='lblIngName')):
         quantity = quantity.contents[0].encode('utf-8').split(' ', 1)
@@ -44,11 +48,35 @@ def Parse():
         if len(quantity) > 1:
             newIngredient.measurement = quantity[1]
         newIngredient.name = (ingredient.contents)[0].encode('utf-8')
+        for liquid in liquid_list:
+            if liquid.lower() in newIngredient.name.lower():
+                newIngredient.itype = 'liquid'
+                break
+        if newIngredient.itype == '':
+            for spice in spice_list:
+                if spice.lower() in newIngredient.name.lower():
+                    newIngredient.itype = 'spice'
+                    break
+        if newIngredient.itype == '':
+            for veg in veg_list:
+                if veg.lower() in newIngredient.name.lower():
+                    newIngredient.itype = 'veggie'
+                    break
+        if newIngredient.itype == '':
+            for oil in oil_list:
+                if oil.lower() in newIngredient.name.lower():
+                    newIngredient.itype = 'oil'
+                    break
+        if newIngredient.itype == '':
+            for meat in meat_list:
+                if meat.lower() in newIngredient.name.lower():
+                    newIngredient.itype = 'meat'
+                    break
         recipe.ingredients.append(newIngredient)
         #ingredients.append(((quantity.contents)[0].encode('utf-8'), (ingredient.contents)[0].encode('utf-8')))
 
     for ingredient in recipe.ingredients:
-        print ingredient.quantity, ingredient.measurement, ingredient.name
+        print ingredient.quantity, ingredient.measurement, ingredient.name, ",", ingredient.itype
 
     directions_html = soup.find_all(class_='directLeft')
     directions_span = directions_html[0].select('span')
@@ -56,7 +84,7 @@ def Parse():
         direction = re.sub(r'<.*?>', "", str(direction))
         recipe.directions.append(direction)
 
-    print recipe.directions
+    ##print recipe.directions
 
     for direction in recipe.directions:
         for sentence in direction.split('.'):
@@ -68,15 +96,47 @@ def Parse():
                 sentence2 = sentence.lower()
                 if word in sentence2 and word not in recipe.preparation_methods:
                     recipe.preparation_methods.append(word)
+            for tool in tool_list:
+                sentence2 = sentence.lower()
+                tool = tool.lower()
+                if tool in sentence2 and tool not in recipe.tools:
+                    recipe.tools.append(tool)
 
     print recipe.cooking_methods
     print recipe.preparation_methods
+    print recipe.tools
 
-def ToVeg():
+def toVeg(recipe):
+    if isVeg(recipe):
+        print "Recipe is already vegetarian."
+        return
+    else:
+        meatveg = open('toVeg.txt', 'rb').read().split('\r\n')
+        for ingredient in recipe.ingredients:
+            if ingredient.itype == 'meat':
+                print "replace with veg"
+            elif ingredient.itype == 'liquid' and 'vegetable' not in ingredient.name.lower():
+                print "replace soup stocket with vegetable"
+        print "not done yet"
+
+def toMeat(recipe):
+    vegmeat = open('toMeat.txt', 'rb').read().split('\r\n')
     print "not done yet"
 
-def FromVeg():
-    print "not done yet"
+def isVeg(recipe):
+    for ingredient in recipe.ingredients:
+        if ingredient.itype is 'meat' or (ingredient.itype is 'liquid' and 'vegetable' not in ingredient.name):
+            return False
+    return True
+
+def main():
+    link = raw_input("What is the URL for the recipe? ")
+    recipe = Recipe()
+    parse(link, recipe)
+    toVeg(recipe)
+    toMeat(recipe)
+
+main()
 
 #text = nltk.word_tokenize("chopped fresh parsley")
 #print nltk.pos_tag(text)
