ceb70b96b43971d37a735a92a4b6c63262f8477f
Yuchen Yang <yuchenyang@Yuchen-Yangs-MacBook-Pro.local>
2014-03-19 15:56:51 -0500

    cuisineTransformation major update

--- cuisineTransformation.py
+++ cuisineTransformation.py
@@ -1,239 +1,273 @@
-﻿eastasianToFrench = {
-    'soy sauce':,
-    'rice vinegar':
-    'fish sauce':
-    'chili sauce':
-    'oyster sauce':
-    'coconut milk':
-    'curry paste':
-    'miso paste':
-    'sesame oil':
-    'rice wine':
-    'ginger':
-    'lime':
-    'cilantro':
-    'black bean sauce':
-    'five-spice powder':
-    'garlic':
-    'black pepper':
-    'star anise':
-    'fennel seed':
-    'clove':
-    'cinnamon':
-    'cardamon':
-    'nutmeg':
-    'coriander':
-    'white pepper':
-    'lemongrass':
-    'mint':
-    'salt':
-    'vinegar':
-    'yuzu':
-    'sesame seed':
-    'perilla':
-    'trefoil':
-    'red pepper':
-    'green onion':
+﻿eastasianSauce = {
+    'Doubanjiang',
+    'sweet bean sauce',
+    'hoisin sauce',
+    'oyster sauce',
+    'pouzu',
+    'mala sauce',
+    'soy sauce',
+    'sriracha sauce',
+    'ssamjang',
+    'XO sauce',
+    'lobster sauce',
+    'shacha sauce',
+    'siu haau sauce',
+    'sweet and sour sauce',
+    'tianmianjiang',
+    'teriyaki',
+    'budu',
+    'fish cause',
+    'garum',
+    'nam chim',
+    'nam phrik',
+    'padaek',
+    'pla ra',
+    'sambal',
+    'satay sauce',
+    'peanut sauce',
+    'sweet soy sauce',
+    'tentsuyu'
 }
 
-eastasianToItalian = {
-    'soy sauce':,
-    'rice vinegar':
-    'fish sauce':
-    'chili sauce':
-    'oyster sauce':
-    'coconut milk':
-    'curry paste':
-    'miso paste':
-    'sesame oil':
-    'rice wine':
-    'ginger':
-    'lime':
-    'cilantro':
-    'black bean sauce':
-    'five-spice powder':
-    'garlic':
-    'black pepper':
-    'star anise':
-    'fennel seed':
-    'clove':
-    'cinnamon':
-    'cardamon':
-    'nutmeg':
-    'coriander':
-    'white pepper':
-    'lemongrass':
-    'mint':
-    'salt':
-    'vinegar':
-    'yuzu':
-    'sesame seed':
-    'perilla':
-    'trefoil':
-    'red pepper':
-    'green onion':
+eastasainSpice = {
+    'chilli',
+    'chinese chives',
+    'cassia bark',
+    'fresh coriander',
+    'curry leaves',
+    'fenugreek',
+    'galangal',
+    'ginger',
+    'golden needle',
+    'kaffir lime leaves',
+    'lemongrass',
+    'star anise',
+    'thai basil',
+    'cardamons',
+    'cloves',
+    'coriander',
+    'cumin',
+    'fenugreek seeds',
+    'five spice',
+    'nigella',
+    'seven spice',
+    'turmeric'
 }
 
-frenchToEastasian = {
-    'Poultry':
-    'salt':
-    'pepper':
-    'mustard':
-    'paprika':
-    'garlic':
-    'ginger':
-    'thyme':
-    'basil':
-    'bay leaf':
-    'cayenne pepper':
-    'mustard':
-    'garlic':
-    'parsley':
-    'cumin':
-    #red meat
-    'bouquet garni':
-    'rosemary':
-    'saffron':
-    'cayenne':
-    'tarragon':
-    'mint':
-    'basil':
-    'parsley':
-    'coriander':
-    'cloves':
-    'laurel':
-    'nutmeg':
-    #seafood
-    'paprika':
-    'white pepper':
-    'mixture of thyme, marjoram, rosemary and savory':
-    'mixture of tarragon, chervil, fennel and lavender':
-    #vegetable
-    'clove':
-    'chives':
-    'thyme and bay leaf (potatoes)':
-    'brewer’s yeast':
-    'oregano':
-    #fruit
-    'mint':
-    'vanilla':
-    'cinnamon':
-    'rum extract':
-    #dessert
-    'vanilla':
-    'almond extract':
-    'lemon and orange zest':
-    'nutmeg':
-    'cardamom':
-    #french-canadian
-    'allspice':
-    'onion powder':
-    'sage':
+eastasianOil = {
+    'sesame oil',
+    'lard',
+    'olive oil',
+    'corn oil',
+    'canola oil',
+    'corn oil',
+    'peanut oil',
+    'soybean oil',
+    'walnut oil
 }
 
-frechToItalian = {
-    'Poultry':
-    'salt':
-    'pepper':
-    'mustard':
-    'paprika':
-    'garlic':
-    'ginger':
-    'thyme':
-    'basil':
-    'bay leaf':
-    'cayenne pepper':
-    'mustard':
-    'garlic':
-    'parsley':
-    'cumin':
-    #red meat
-    'bouquet garni':
-    'rosemary':
-    'saffron':
-    'cayenne':
-    'tarragon':
-    'mint':
-    'basil':
-    'parsley':
-    'coriander':
-    'cloves':
-    'laurel':
-    'nutmeg':
-    #seafood
-    'paprika':
-    'white pepper':
-    'mixture of thyme, marjoram, rosemary and savory':
-    'mixture of tarragon, chervil, fennel and lavender':
-    #vegetable
-    'clove':
-    'chives':
-    'thyme and bay leaf (potatoes)':
-    'brewer’s yeast':
-    'oregano':
-    #fruit
-    'mint':
-    'vanilla':
-    'cinnamon':
-    'rum extract':
-    #dessert
-    'vanilla':
-    'almond extract':
-    'lemon and orange zest':
-    'nutmeg':
-    'cardamom':
-    #french-canadian
-    'allspice':
-    'onion powder':
-    'sage':
+eastasianLiquid = {
+    'rice vinegar',
+    'vinegar',
+    'cooking wine',
+    'chicken stock'
 }
 
-italianToEastasian = {
-    'coriander':
-    'ginger':
-    'nutmeg':
-    'black pepper':
-    'white pepper':
-    'saffon':
-    'salt':
-    'vanilla':
-    'basil':
-    'bay leaves':
-    'borage':
-    'ennel':
-    'juniper':
-    'marjoram, Sweet':
-    'myrtle':
-    'oregano':
-    'parsley':
-    'rosemary':
-    'sage':
-    'olive oil':
-    'garlic':
-    'thyme':
+eastasianVegetable = {
+    'napa cabbage',
+    'chinese flowering cabbage',
+    'chinese broccoli',
+    'bamboo shoot',
+    'beansprout',
+    'daikon',
+    'garlic',
+    'chinese white cabbage',
+    'thai eggplant',
+    'cherry egg plant',
+    'yard long bean',
+    'winged bean',
+    'water chestnut',
+    'chinese dried mushroom',
+    'straw mushroom',
+    'potato',
+    'tomato',
+    'onion',
+    'green pepper',
+    'red pepper',
 }
 
-italianToFrench = {
-    'coriander':
-    'ginger':
-    'nutmeg':
-    'black pepper':
-    'white pepper':
-    'saffon':
-    'salt':
-    'vanilla':
-    'basil':
-    'bay leaves':
-    'borage':
-    'ennel':
-    'juniper':
-    'marjoram, Sweet':
-    'myrtle':
-    'oregano':
-    'parsley':
-    'rosemary':
-    'sage':
-    'olive oil':
-    'garlic':
-    'thyme':
+frenchSauce = {
+    'Sauce bechamel',
+    'Sauce espagnole',
+    'sauce veloute',
+    'sauce hollandaise',
+    'sauce tomate',
+    'Allemande sauce',
+    'Au jus',
+    'Sauce Bourguignonne',
+    'Breton sauce',
+    'Beurre noir',
+    'Beurre noisette',
+    'Sauce charcutière',
+    'Chasseur',
+    'Nantua sauce',
+    'Ravigote',
+    'Rouennaise', 
+    'Rouille',
+    'Sauce gribiche',
+    'Sauce lyonnaise',
+    'Sauce poivrade',
+    'Soubise sauce',
+    'Velouté sauce'
 }
+
+frenchSpice = {
+    'mustard',
+    'paprika',
+    'garlic',
+    'ginger',
+    'thyme',
+    'basil',
+    'bay leaf',
+    'cayenne pepper',
+    'parsley',
+    'cumin',
+    'bouquet garni',
+    'rosemary',
+    'saffron',
+    'cayenne',
+    'tarragon',
+    'mint',
+    'basil',
+    'coriander',
+    'cloves',
+    'laurel',
+    'nutmeg',
+    'vanilla',
+    'almond extract',
+    'onion powder',
+    'sage'
+}
+
+frenchOil = {
+    'almond oil',
+    'avocado oil',
+    'butter',
+    'canola oil',
+    'coconut oil',
+    'corn oil',
+    'lard',
+    'mustard oil',
+    'olive oil',
+    'palm oil',
+    'peanut oil',
+    'walnut oil'
+}
+
+frenchLiquid = {
+    'chicken stock',
+    'fish stock',
+    'beef broth',
+    'chicken broth',
+    'vegetable broth'
+}
+
+frenchVegetable = {
+    'artichoke',
+    'asparagus',
+    'eggplant',
+    'carrot',
+    'elery',
+    'mushroom',
+    'cauliflower',
+    'cucumber',
+    'spinach',
+    'green bean',
+    'onion',
+    'red pepper',
+    'green pepper',
+    'potato',
+    'tomato'
+}
+
+italianSauce = {
+    'amatriciana sauce',
+    'arrabbiata sauce',
+    'bechamel sauce',
+    'bigoli in salsa',
+    'bolognese sauce',
+    'cacio e pepe',
+    'carbonara',
+    'clam sauce',
+    'genovese sauce',
+    'green sauce',
+    'gremolata',
+    'marinara sauce',
+    'neapolitan ragu',
+    'pesto',
+    'ragu',
+    'salmoriglio',
+    'sugo alla puttanesca',
+    'tomato sauce',
+    'vodka sauce'
+}
+
+italianSpice = {
+    'garlic',
+    'bay leaves',
+    'basil',
+    'onion',
+    'fennel',
+    'mint',
+    'calamintha nepeta',
+    'oregano',
+    'chili pepper',
+    'parsley',
+    'rosemary',
+    'sage',
+    'thyme'
+}
+
+italianOil = {
+    'almond oil',
+    'avocado oil',
+    'butter',
+    'canola oil',
+    'coconut oil',
+    'corn oil',
+    'lard',
+    'mustard oil',
+    'olive oil',
+    'palm oil',
+    'peanut oil',
+    'walnut oil'
+}
+italianLiquid = {
+    'chicken stock',
+    'fish stock',
+    'beef broth',
+    'chicken broth',
+    'vegetable broth'
+}
+italianVegetable = {
+    'tomato',
+    'garlic',
+    'onion',
+    'bell pepper',
+    'eggplant',
+    'cabbage',
+    'zucchini',
+    'artichokes',
+    'fennel',
+    'mushroom',
+    'celery',
+    'asparagus',
+    'broccoli',
+    'spinach',
+    'cauliflower',
+    'lettuce'
+}
+
+
+
+
