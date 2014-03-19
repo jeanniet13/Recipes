eafb227bf2fa25a26ec5756b15a7532a9b220130
Yuchen Yang <yuchenyang@Yuchen-Yangs-MacBook-Pro.local>
2014-03-19 13:02:50 -0500

    added cuisine transformation file

---
+++ cuisineTransformation.py
@@ -0,0 +1,239 @@
+﻿eastasianToFrench = {
+    'soy sauce':,
+    'rice vinegar':
+    'fish sauce':
+    'chili sauce':
+    'oyster sauce':
+    'coconut milk':
+    'curry paste':
+    'miso paste':
+    'sesame oil':
+    'rice wine':
+    'ginger':
+    'lime':
+    'cilantro':
+    'black bean sauce':
+    'five-spice powder':
+    'garlic':
+    'black pepper':
+    'star anise':
+    'fennel seed':
+    'clove':
+    'cinnamon':
+    'cardamon':
+    'nutmeg':
+    'coriander':
+    'white pepper':
+    'lemongrass':
+    'mint':
+    'salt':
+    'vinegar':
+    'yuzu':
+    'sesame seed':
+    'perilla':
+    'trefoil':
+    'red pepper':
+    'green onion':
+}
+
+eastasianToItalian = {
+    'soy sauce':,
+    'rice vinegar':
+    'fish sauce':
+    'chili sauce':
+    'oyster sauce':
+    'coconut milk':
+    'curry paste':
+    'miso paste':
+    'sesame oil':
+    'rice wine':
+    'ginger':
+    'lime':
+    'cilantro':
+    'black bean sauce':
+    'five-spice powder':
+    'garlic':
+    'black pepper':
+    'star anise':
+    'fennel seed':
+    'clove':
+    'cinnamon':
+    'cardamon':
+    'nutmeg':
+    'coriander':
+    'white pepper':
+    'lemongrass':
+    'mint':
+    'salt':
+    'vinegar':
+    'yuzu':
+    'sesame seed':
+    'perilla':
+    'trefoil':
+    'red pepper':
+    'green onion':
+}
+
+frenchToEastasian = {
+    'Poultry':
+    'salt':
+    'pepper':
+    'mustard':
+    'paprika':
+    'garlic':
+    'ginger':
+    'thyme':
+    'basil':
+    'bay leaf':
+    'cayenne pepper':
+    'mustard':
+    'garlic':
+    'parsley':
+    'cumin':
+    #red meat
+    'bouquet garni':
+    'rosemary':
+    'saffron':
+    'cayenne':
+    'tarragon':
+    'mint':
+    'basil':
+    'parsley':
+    'coriander':
+    'cloves':
+    'laurel':
+    'nutmeg':
+    #seafood
+    'paprika':
+    'white pepper':
+    'mixture of thyme, marjoram, rosemary and savory':
+    'mixture of tarragon, chervil, fennel and lavender':
+    #vegetable
+    'clove':
+    'chives':
+    'thyme and bay leaf (potatoes)':
+    'brewer’s yeast':
+    'oregano':
+    #fruit
+    'mint':
+    'vanilla':
+    'cinnamon':
+    'rum extract':
+    #dessert
+    'vanilla':
+    'almond extract':
+    'lemon and orange zest':
+    'nutmeg':
+    'cardamom':
+    #french-canadian
+    'allspice':
+    'onion powder':
+    'sage':
+}
+
+frechToItalian = {
+    'Poultry':
+    'salt':
+    'pepper':
+    'mustard':
+    'paprika':
+    'garlic':
+    'ginger':
+    'thyme':
+    'basil':
+    'bay leaf':
+    'cayenne pepper':
+    'mustard':
+    'garlic':
+    'parsley':
+    'cumin':
+    #red meat
+    'bouquet garni':
+    'rosemary':
+    'saffron':
+    'cayenne':
+    'tarragon':
+    'mint':
+    'basil':
+    'parsley':
+    'coriander':
+    'cloves':
+    'laurel':
+    'nutmeg':
+    #seafood
+    'paprika':
+    'white pepper':
+    'mixture of thyme, marjoram, rosemary and savory':
+    'mixture of tarragon, chervil, fennel and lavender':
+    #vegetable
+    'clove':
+    'chives':
+    'thyme and bay leaf (potatoes)':
+    'brewer’s yeast':
+    'oregano':
+    #fruit
+    'mint':
+    'vanilla':
+    'cinnamon':
+    'rum extract':
+    #dessert
+    'vanilla':
+    'almond extract':
+    'lemon and orange zest':
+    'nutmeg':
+    'cardamom':
+    #french-canadian
+    'allspice':
+    'onion powder':
+    'sage':
+}
+
+italianToEastasian = {
+    'coriander':
+    'ginger':
+    'nutmeg':
+    'black pepper':
+    'white pepper':
+    'saffon':
+    'salt':
+    'vanilla':
+    'basil':
+    'bay leaves':
+    'borage':
+    'ennel':
+    'juniper':
+    'marjoram, Sweet':
+    'myrtle':
+    'oregano':
+    'parsley':
+    'rosemary':
+    'sage':
+    'olive oil':
+    'garlic':
+    'thyme':
+}
+
+italianToFrench = {
+    'coriander':
+    'ginger':
+    'nutmeg':
+    'black pepper':
+    'white pepper':
+    'saffon':
+    'salt':
+    'vanilla':
+    'basil':
+    'bay leaves':
+    'borage':
+    'ennel':
+    'juniper':
+    'marjoram, Sweet':
+    'myrtle':
+    'oregano':
+    'parsley':
+    'rosemary':
+    'sage':
+    'olive oil':
+    'garlic':
+    'thyme':
+}
