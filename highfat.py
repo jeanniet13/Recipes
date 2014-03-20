6f954660570de0418271fefb530cdae4621dcca0
jeanniet13 <jeanniet13@gmail.com>
2014-03-20 15:01:43 -0500

    Update highfat.py

--- highfat.py
+++ highfat.py
@@ -1,30 +1,32 @@
 highfatgrain = [
-  white rice
-  pasta
-  bread
+  "white rice",
+  "pasta",
+  "bread"
 ]
 
+/* not useful; check cooking method instead?
 highfatveg = [
   fried potatoes
   roasted potatoes
   roasted peanuts
   french fries
 ]
+*/
 
 highfatmeat = [
-  fish roe
-  caviar
-  sausage
-  pate
-  duck
-  goose
-  meat pies
+  "fish roe",
+  "caviar",
+  "sausage",
+  "pate",
+  "duck",
+  "goose",
+  "meat pie"
 ]
 
 highfatliquid = [
-  whole milk
-  cream
-  butter
-  margarine
-  vegetable oil
+  #whole milk
+  "cream",
+  "butter",
+  "margarine",
+  "vegetable oil",
 ]
